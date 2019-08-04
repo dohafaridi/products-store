02b2320b603b8c41c55fe540aac3529f9ed558aa
Doha Faridi <dohafaridi@gmail.com>
2019-08-04 17:16:22 +0200

    Client setup and add HeaderNav component

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import HeaderNav from "./components/HeaderNav";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/HeaderNav.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
+
+const HeaderNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Navbar color="dark" dark expand="sm" className="mb-5">
+      <Container>
+        <NavbarBrand href="/">Products store</NavbarBrand>
+        <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/dohafaridi/products-store">
+                Github
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default HeaderNav;
