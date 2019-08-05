a5e7c0d3cbfa058358d2c5bdcffc9e6388aff977
Doha Faridi <dohafaridi@gmail.com>
2019-08-05 23:14:57 +0200

    Products components with dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import HeaderNav from "./components/HeaderNav";
+import Products from "./components/Products";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <HeaderNav />
+      <Products />
     </div>
   );
 }
