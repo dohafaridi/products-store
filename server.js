a80617e036e72eef72679ed0f81329778b05ba7e
Doha Faridi <dohafaridi@gmail.com>
2019-08-12 00:07:04 +0200

    Prepare the app for deployment to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const productRoutes = require("./routes/api/product");
 const mongodbURL = require("./config/keys").mongodbURL;
@@ -18,6 +19,16 @@ mongoose
 
 // Routes
 app.use("/api/products", productRoutes);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
